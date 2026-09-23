503edc41169c2cc263ef0bb12f6f63182e5ebdac
Lucas Libshutz <lsl94@cornell.edu>
2026-09-23 10:34:46 -0400

    propagation model tests

--- src/ekf/model.rs
+++ src/ekf/model.rs
@@ -114,3 +114,83 @@ pub fn predict(
     (propagate(x, omega_z, dt), f * p * f.transpose() + qd)
 }
 
+
+#[cfg(test)]
+mod tests {
+    use approx::assert_relative_eq;
+    use super::*;
+    use std::f64::consts::{FRAC_PI_2, PI, TAU};
+
+    #[test]
+    fn enum_is_contiguous() {
+        for (n,s) in S::ALL.iter().enumerate() {
+            assert_eq!(s.ix(), n);
+        }
+    }
+
+    #[test]
+    fn jacobian_matches_finite_diff() {
+        let x = Vec5::new(10.0, -5.0, 0.7, 12.0, 0.01);
+        let (omega, dt, h) = (0.15, 0.02, 1e-6);
+
+        let f = jacobian(&x, dt);
+
+        for col in S::ALL {
+            let (mut hi, mut lo) = (x,x);
+            hi[col] += h;
+            lo[col] -= h;
+            let fd = (propagate(&hi, omega, dt) - propagate(&lo, omega, dt)) / (2.0 * h);
+
+            for row in S::ALL {
+                assert_relative_eq!(f.at(row, col), fd[row], epsilon=1e-6);
+            }
+        }
+    }
+
+    #[test]
+    fn straight_line_is_v_t() {
+        let b = 0.01;
+        let x0 = Vec5::new(0.0, 0.0, 0.0, 10.0, b);
+        let (dt, n) = (0.01, 500);
+
+        let mut x = x0;
+        for _ in 0..n {
+            x = propagate(&x, b, dt)
+        }
+
+        assert_relative_eq!(x[S::Pe], 10.0 * dt * n as f64, epsilon=1e-9);
+        assert_relative_eq!(x[S::Pn], 0.0, epsilon=1e-12);
+        assert_relative_eq!(x[S::Psi], 0.0, epsilon=1e-12);
+        assert_relative_eq!(x[S::V], 10.0, epsilon=1e-12);
+        assert_relative_eq!(x[S::Bw], b, epsilon=1e-12);
+    }
+
+    #[test]
+    fn check_pi2_rotation_from_east_to_north() {
+        let x = Vec5::new(0.0, 0.0, FRAC_PI_2, 10.0, 0.0);
+        let out = propagate(&x, 0.0, 0.1);
+        assert_relative_eq!(out[S::Pe], 0.0, epsilon=1e-12);
+        assert_relative_eq!(out[S::Pn], 1.0, epsilon=1e-12);
+    }
+
+    #[test]
+    fn predict_yields_symmetric_covariance() {
+        let x = Vec5::new(1.0, 2.0, 0.4, 8.0, 2e-3);
+        let p = Mat5::from_diagonal(&Vec5::new(4.0, 4.0, 0.1, 1.0, 1e-4));
+        let (_, pp) = predict(&x, &p, 0.1, 0.01, &ProcessNoise::default());
+
+        assert_relative_eq!(pp, pp.transpose(), epsilon=1e-12);
+        assert!(pp.at(S::Pe, S::Pe) > p.at(S::Pe, S::Pe));
+    }
+
+    #[test]
+    fn wrap_pi_is_periodic() {
+        for &a in &[0.0, 0.3, -0.3, 3.0, -3.0, FRAC_PI_2, -FRAC_PI_2] {
+            assert_relative_eq!(wrap_pi(a), a, epsilon=1e-12);
+            assert_relative_eq!(wrap_pi(a + TAU), a, epsilon=1e-12);
+            assert_relative_eq!(wrap_pi(a - TAU), a, epsilon=1e-12);
+            assert_relative_eq!(wrap_pi(wrap_pi(a)), a, epsilon=1e-12);
+        }
+        assert_relative_eq!(wrap_pi(-PI - 0.1), PI - 0.1, epsilon=1e-12);
+    }
+}
